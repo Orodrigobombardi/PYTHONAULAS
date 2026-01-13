faf3d23c301bd896986bd04adeda87f79881b000
Rodrigo Bombardi <bombardifilhorodrigo@gmail.com>
2026-01-13 00:35:18 -0300

    PYTHON ATT DE AULAS . FODASE
    FODASE

---
+++ ifelif3.py
@@ -0,0 +1,31 @@
+condicao = input("Digite algo que seja verdadeiro: ")
+if condicao == "verdadeiro":
+    print ("A condição é realmente verdadeira!")
+if condicao == "falso":
+    print ("A condição é falsa!")
+if  condicao == "talvez" : 
+    print('toma no teu cu ')
+
+
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+
+if condicao1: 
+    print("verdadeiro1")
+elif condicao1 : 
+    print ("sapoha é falsa")
+if condicao2: 
+    print("verdadeiro2")
+elif condicao2 : 
+    print ("sapoha é falsa")
+if condicao3: 
+    print("verdadeiro3")
+elif condicao3 : 
+    print ("sapoha é falsa")
+else : 
+    print("todas são falsas")
+
+
+
