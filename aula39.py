fb7a26a137a537dfc4f543aae4339b3a51157bb4
Orodrigobombardi <bombardifilhorodrigo@gmail.com>
2026-01-19 09:12:37 -0300

    hello mycomit

---
+++ aula39.py
@@ -0,0 +1,13 @@
+senha1 = input ('senha')  or 'sem senha'
+
+if senha1 == 123456 : 
+    print('entrou')
+else : 
+    print('n')
+
+    # nesta linha de exemplo eu ja consegui fazer uma verificacao de codigo em apenas uma linha sem precisar fazer if e else . 
+
+
+
+
+     
