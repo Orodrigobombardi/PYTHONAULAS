5a3db39c8bd3cf5ea4b8e333aa8d90d570dbddbc
Rodrigo Bombardi <bombardifilhorodrigo@gmail.com>
2026-02-11 19:35:36 -0300

    att

---
+++ aula52.py
@@ -0,0 +1,35 @@
+# numero = int(input("digite um numero inteiro : "))
+# if numero %2 == 0: 
+#     print ("seu numero é par")
+# else: 
+#     print("seu numero é impar")
+
+
+
+#sep__________________________
+
+
+# horario = int(input("digite um horario / ex: 12 , 13 , 9 , 3 : "))
+
+# if horario < 12: 
+#     print ("bom dia")
+# elif horario < 18: 
+#     print("bom dia")
+# else: 
+#     print("boa noite")
+
+#sep__________________________
+
+op = input("digite o operador que deseja utilizar (+ , - , / , * ) : ")
+num1 = float(input("digite o primeiro numero : "))
+num2 = float(input("digite o segundo numero : "))
+
+if op == ('+'):
+    print (num1 + num2) 
+if op == ('-'):
+    print (num1 - num2) 
+if op == ('*'):
+    print (num1 * num2) 
+if op == ('/'):
+    print (num1 / num2) 
+0
---
+++ aula53.py
@@ -0,0 +1,8 @@
+metros = float(input("insira aqui sua metragem que deseja converter : "))
+centimetros = metros * 100 
+if metros: 
+    print (f"sua conersão de metros para centimetros é {centimetros}" )
+elif metros: 
+    print("digite algo util")
+
+
